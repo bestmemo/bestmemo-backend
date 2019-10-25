ba5c53c475abb16b895c4939e976ff95b9c3b85d
chengluyu <chengluyu@live.cn>
2019-10-25 22:03:25 +0800

    Enable import functionality

--- src/memorization.js
+++ src/memorization.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const pluralize = require('pluralize');
 const { DolphinSR, generateId } = require('dolphinsr');
 const { Card, Review } = require('../src/model');
 const { polishCard } = require('./dictionary');
@@ -5,7 +8,29 @@ const { polishCard } = require('./dictionary');
 const sr = new DolphinSR();
 let currentCard = null;
 
+async function importCards() {
+  const importFilePath = path.join(__dirname, '..', 'data', 'imports.json');
+  if (!fs.existsSync(importFilePath)) {
+    return;
+  }
+  const newCards = JSON.parse(fs.readFileSync(importFilePath, 'utf-8'));
+  let numImported = 0;
+
+  for (const word of newCards) {
+    if (await Card.findOne({ where: { text: word } })) {
+      continue;
+    }
+    await Card.create({ id: generateId(), text: word });
+    numImported += 1;
+  }
+
+  fs.writeFileSync(importFilePath, '[]');
+  console.log(`${numImported} ${pluralize('word', numImported)} had been imported.`);
+}
+
 async function initialize() {
+  await importCards();
+
   const combination = { front: [0], back: [0] };
   const combinations = [combination];
 
--- src/server.js
+++ src/server.js
@@ -1,37 +1,6 @@
-const fs = require('fs');
 const path = require('path');
-
-const pluralize = require('pluralize');
 const fastify = require('fastify');
-
 const memo = require('./memorization');
-
-// function loadData(filePath) {
-//   return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-// }
-
-// const storagePaths = {
-//   cards: path.join(__dirname, '..', 'data', 'cards.json'),
-//   imports: path.join(__dirname, '..', 'data', 'imports.json'),
-//   reviews: path.join(__dirname, '..', 'data', 'reviews.json')
-// }
-
-// const storage = mapValues(storagePaths, loadData)
-
-// // import the cards every time launch
-// let numImported = 0
-// const cardSet = new Set(storage.cards.map(x => x.fields[0]));
-// storage.imports.forEach(word => {
-//   if (!cardSet.has(word)) {
-//     storage.cards.push({
-//       id: generateId(),
-//       combinations: [{ front: [0], back: [0] }],
-//       fields: [word],
-//     })
-//     cardSet.add(word)
-//     numImported += 1
-//   }
-// })
 
 // initialize the server
 const server = fastify({
